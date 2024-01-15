bfb0d7ed0634efbef974b6930d5dc302a1c1968e
techsup69 <sup69.69@mail.ru>
2024-01-16 01:24:59 +0300

    remove some trash

--- src/main/java/com/tmp/servlet/AServlet.java
+++ src/main/java/com/tmp/servlet/AServlet.java
@@ -11,12 +11,8 @@ public class AServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         final String message = "{}";
-        //*.1
         resp.setContentType("application/json;charset=utf-8");
         resp.getWriter().print(message);
-
-        //*.2
-//        resp.getOutputStream().write(message.getBytes(StandardCharsets.UTF_8));
     }
 
 }
